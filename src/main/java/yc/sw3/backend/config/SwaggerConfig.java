49d234fb3c0549ad5f1963d5778cf198965ea6c0
Gri22ly <79156923+Junseung0526@users.noreply.github.com>
2026-04-03 11:50:08 +0900

    setting: swagger docs 현재 모드 반영

--- src/main/java/yc/sw3/backend/config/SwaggerConfig.java
+++ src/main/java/yc/sw3/backend/config/SwaggerConfig.java
@@ -2,18 +2,22 @@ package yc.sw3.backend.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${custom.env-name:Production}")
+    private String envName;
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("프로젝트 API 문서")
-                        .description("Spring Boot 3를 이용한 API 명세서입니다.")
+                        .title("🚀 " + "SWENG3 Backend API")
+                        .description("현재 실행 환경: **" + envName + "**")
                         .version("1.0.0"));
     }
 }
