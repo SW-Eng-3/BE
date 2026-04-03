31067dbd23c3a516cb4a8080f1e21150e0eac040
Gri22ly <79156923+Junseung0526@users.noreply.github.com>
2026-04-03 11:45:57 +0900

    feat: Swagger Setting

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,15 @@
-FROM ubuntu:latest
-LABEL authors="rla00"
+FROM gradle:8.5-jdk17 AS build
 
-ENTRYPOINT ["top", "-b"]
+COPY --chown=gradle:gradle . /home/gradle/src
+
+WORKDIR /home/gradle/src
+
+RUN gradle build --no-daemon -x test
+
+FROM openjdk:17-jdk-slim
+
+EXPOSE 8080
+
+COPY --from=build /home/gradle/src/build/libs/*-SNAPSHOT.jar app.jar
+
+ENTRYPOINT ["java", "-Dspring.profiles.active=prod", "-Dserver.port=${PORT:8080}", "-jar", "/app.jar"]
--- src/main/java/yc/sw3/backend/config/SwaggerConfig.java
+++ src/main/java/yc/sw3/backend/config/SwaggerConfig.java
@@ -1,4 +1,19 @@
 package yc.sw3.backend.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("프로젝트 API 문서")
+                        .description("Spring Boot 3를 이용한 API 명세서입니다.")
+                        .version("1.0.0"));
+    }
 }
