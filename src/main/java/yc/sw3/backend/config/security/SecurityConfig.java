c878cee6b47056cbbc19e6ee960d9335a8d9a6ec
Gri22ly <79156923+Junseung0526@users.noreply.github.com>
2026-06-11 15:33:30 +0900

    cors: 프론트 주소 추가

--- src/main/java/yc/sw3/backend/config/security/SecurityConfig.java
+++ src/main/java/yc/sw3/backend/config/security/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://127.0.0.1:5173", "https://be-y7hs.onrender.com/"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://127.0.0.1:5173", "https://be-y7hs.onrender.com/", "https://mentor-student-connect.onrender.com/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "X-User-Id"));
         configuration.setAllowCredentials(true);
